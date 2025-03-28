ddc96686f6eff34c5a6395227a943d7fb6231722
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-03-29 04:49:18 +0500

    completed todolist

--- practice03/src/App.jsx
+++ practice03/src/App.jsx
@@ -1,12 +1,52 @@
 import { useState } from "react"
 
 function App(){
-  const [list, setList] = useState([])
+  const [lists, setLists] = useState([])
   const [newTask, setNewTask] = useState('')
+  // let updatedList = []
+
+  function addItem(){
+    if(newTask.trim() !== ''){
+      setLists([...lists, newTask])
+      setNewTask('')
+    }
+  }
+
+  function removeList(index){
+    setLists(lists.filter((_, i) => i !== index))
+  }
+
+  function moveUp(index){
+    if(index > 0){
+       let updatedList = [...lists]
+      [updatedList[index], updatedList[index - 1]] = [updatedList[index - 1], updatedList[index]]
+      setLists(updatedList)
+    }
+  }
+
+  function moveDown(index){
+    if(index < lists.length - 1){
+       let updatedList = [...lists]
+      [updatedList[index], updatedList[index + 1]] = 
+      [updatedList[index + 1], updatedList[index]]
+      setLists(updatedList)
+    }
+  }
 
   return(
     <>
       <h1>ToDo List React</h1>
+      <input value={newTask} type="text" placeholder="Enter todo work" onChange={e => setNewTask(e.target.value)} />
+      <button onClick={addItem}>Add</button>
+      <ul>
+        {lists.map((list, index) => 
+        <li key={index}>{list} 
+        <button onClick={() => removeList(index)}>Remove</button>
+        <button onClick={() => moveUp(index)}>up</button>
+        <button onClick={() => moveDown(index)}>down</button>
+        </li>
+        )}
+      </ul>
     </>  
   )
 
