a4813d15f7501848378534f3f7f05968297a72e9
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-03-29 05:08:43 +0500

    completed timer clock in react

---
+++ practice04/src/DigitalClock.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+function DigitalClock(){
+
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+        let intervalId = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+
+        return(() => {
+            clearInterval(intervalId)
+        })
+    })
+
+    function formatTime(){
+        const hour = time.getHours()
+        const min = time.getMinutes()
+        const sec = time.getSeconds()
+        const amPm = hour > 12 ? 'PM' : "AM"
+
+        let perfectHour = hour % 12 || 12
+
+        return `${padZero(perfectHour)} : ${padZero(min)} : ${padZero(sec)} ${amPm}`
+    }
+
+    function padZero(number){
+       return number > 12 ? '' + number : '0' + number
+    }
+
+    return (
+        <>
+            <h1>Time in React</h1>
+            <h2>{formatTime()}</h2>
+        </>
+    )
+
+    
+}
+export default DigitalClock
