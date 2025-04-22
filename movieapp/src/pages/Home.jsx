e276c521154541927b7d6b944f5a9880c036bc3f
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-23 00:39:14 +0500

    made search functional

--- movieapp/src/pages/Home.jsx
+++ movieapp/src/pages/Home.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MovieCard from '../components/MovieCard'
 
 const Home = () => {
     const movies = [
         {id: 1, title: 'Inception', year: 2010, genre: 'Sci-Fi'},
         {id: 2, title: 'The Dark Knight', year: 2008, genre: 'Action'},
-
+        {id: 3, title: 'Interstellar', year: 2014, genre: 'Sci-Fi'}
     ]
+    const [query, setQuery] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
   return (
     <>
-     <header>
-        <input type="text" placeholder='Enter movie to search' />
-        <button>Search</button>    
-    </header> 
-    <main>
-        {movies.map(movie => (
-            <MovieCard movie={movie} key={movie.id} />
-        ))}
-    </main>
+    <form onSubmit={handleSubmit}>
+        <header>
+            <input type="text" placeholder='Enter movie to search' value={query} onChange={(e) => setQuery(e.target.value)} />
+            <button>Search</button>    
+        </header> 
+    </form>
+        <main>
+            {movies.map(movie => (
+                movie.title.toLowerCase().startsWith(query) && (
+                <MovieCard movie={movie} key={movie.id} /> )
+            ))}
+        </main>
     </>
   )
 }
