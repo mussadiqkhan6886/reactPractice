d569b0854b1f800f56658eee8b11800f69241ff8
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-22 20:54:34 +0500

    started movieapp from home

--- movieapp/src/components/MovieCard.jsx
+++ movieapp/src/components/MovieCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({movie}) => {
   return (
     <div>
-      
+      <div>
+        <img src="" alt={movie.title} />
+      </div>
+      <div>
+        <button>♥</button>
+      </div>
+      <div>
+        <h1>{movie.title}</h1>
+        <h2>{movie.release_date}</h2>
+      </div>
     </div>
   )
 }
--- movieapp/src/pages/Home.jsx
+++ movieapp/src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import MovieCard from '../components/MovieCard'
 
 const Home = () => {
+    const movies = [
+        {id: 1, title: 'Inception', year: 2010, genre: 'Sci-Fi'},
+        {id: 2, title: 'The Dark Knight', year: 2008, genre: 'Action'},
+
+    ]
   return (
-    <div>
-      
-    </div>
+    <>
+     <header>
+        <input type="text" placeholder='Enter movie to search' />
+        <button>Search</button>    
+    </header> 
+    <main>
+        {movies.map(movie => (
+            <MovieCard movie={movie} key={movie.id} />
+        ))}
+    </main>
+    </>
   )
 }
 
