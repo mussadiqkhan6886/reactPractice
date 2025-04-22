08dd3b42370cdca30e307a8e09843ff13c718e64
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-23 03:20:16 +0500

    started react router

--- movieapp/src/pages/Fav.jsx
+++ movieapp/src/pages/Fav.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Fav = () => {
   return (
     <div>
-      
+      <h1>No Favorite movies yet.</h1>
+      <p>Start adding Favorites movies and it will appear here</p>
     </div>
   )
 }
