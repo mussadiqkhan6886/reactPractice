d09c4437767f1bc4b1c381e17735e645381fc6b3
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-23 15:31:00 +0500

    updateed movie card hover

--- movieapp/src/components/MovieCard.jsx
+++ movieapp/src/components/MovieCard.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MovieCard = ({movie}) => {
+  
+  const [mouse, setMouse] = useState(false);
+
+  const handleMouseIn = () => {
+    setMouse(true);
+  }
+
+  const handleMouseOut = () => {
+    setMouse(false);
+  }
+
+  const handleFav = () => [
+    alert('cliked')
+  ]
+
   return (
-    <div className='bg-gray-900 flex flex-col p-4'>
-      <div className='w-full h-[350px]'>
+    <div className='bg-gray-900 flex flex-col relative'>
+      <div onMouseOver={handleMouseIn} onMouseLeave={handleMouseOut} className={`w-full h-[350px] ${mouse ? 'bg-black' : ''} `}>
         <img src="" alt={movie.title} />
       </div>
-      <div>
+      <div onMouseOver={handleMouseIn} onMouseLeave={handleMouseOut} onClick={handleFav} className={`absolute top-1 right-3 ${mouse ? "block" : "hidden"} `}>
         <button className='text-white text-2xl'>♥</button>
       </div>
-      <div>
+      <div className='p-4'>
         <h1 className='text-white'>{movie.title}</h1>
         <h2 className='text-gray-500'>{movie.year}</h2>
       </div>
