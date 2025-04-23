b348b34760dc36d9d78ddd7c806aed7b96126529
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-24 01:49:04 +0500

    done with searching

--- movieapp/src/pages/Home.jsx
+++ movieapp/src/pages/Home.jsx
@@ -26,9 +26,25 @@ const Home = () => {
         getMovies();
     }, [])
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
         e.preventDefault();
-        setQuery('')
+
+        if(!query.trim()) return;
+        if(loading) return;
+
+        setLoading(true);
+
+        try{
+            const movieSearch = await searchMovie(query);
+            setMovies(movieSearch)
+            setError(null)
+        }catch(err){
+            console.log(err);
+            setError("failed to search movie.")
+        }finally{
+            setLoading(false);
+        }
+
     }
 
   return (
@@ -36,14 +52,17 @@ const Home = () => {
     <form className='w-full' onSubmit={handleSubmit}>
         <div className='flex gap-5 mb-5 w-full items-center justify-center'>
             <input className='bg-gray-600 border-none text-white outline-0 px-3 w-full py-2 placeholder:text-gray-400 text-[14px]' type="text" placeholder='Search movie' value={query} onChange={(e) => setQuery(e.target.value)} />
-            <button className='bg-red-600 text-white px-5 py-1.5 border-none '>Search</button>    
+            <button type='submit' className='bg-red-600 cursor-pointer text-white px-5 py-1.5 border-none '>Search</button>    
         </div> 
     </form>
-        <div className='flex flex-col gap-7 w-full sm:grid sm:grid-cols-2 md:grid-cols-3'>
-            {movies.map(movie => (
-                <MovieCard movie={movie} key={movie.id} /> )
-            )}
-        </div>
+
+    {error && <div>{error}</div>}
+
+    {loading ? ( <div>Loading...</div> ) : ( <div className='flex flex-col gap-7 w-full sm:grid sm:grid-cols-2 md:grid-cols-3 cursor-pointer'> 
+        {movies.map(movie => (
+            <MovieCard movie={movie} key={movie.id} /> )
+        )}
+    </div>)}
     </>
   )
 }
