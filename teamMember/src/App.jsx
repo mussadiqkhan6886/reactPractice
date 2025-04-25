603082b78eafbee29caa72c5c093812cb957e602
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-25 20:40:23 +0500

    updated employee and header file

--- teamMember/src/App.jsx
+++ teamMember/src/App.jsx
@@ -2,12 +2,110 @@ import React from 'react'
 import Header from './Header'
 import Employee from './Employee'
 import Footer from './Footer'
+import { useState } from 'react'
 
 const App = () => {
+  
+  const [selectedTeam, setSelectedTeam] = useState("TeamB")
+  const [employees, setEmployees] = useState([{
+
+    id: 1,
+    fullName: "Bob Jones",
+    designation: "JavaScript Developer",
+    gender: "male",
+    teamName: "TeamA"
+  },
+  {
+    id: 2,
+    fullName: "Jill Bailey",
+    designation: "Node Developer",
+    gender: "female",
+    teamName: "TeamA"
+  },
+  {
+    id: 3,
+    fullName: "Gail Shepherd",
+    designation: "Java Developer",
+    gender: "female",
+    teamName: "TeamA"
+  },
+  {
+    id: 4,
+    fullName: "Sam Reynolds",
+    designation: "React Developer",
+    gender: "male",
+    teamName: "TeamB"
+  },
+  {
+    id: 5,
+    fullName: "David Henry",
+    designation: "DotNet Developer",
+    gender: "male",
+    teamName: "TeamB"
+  },
+  {
+    id: 6,
+    fullName: "Sarah Blake",
+    designation: "SQL Server DBA",
+    gender: "female",
+    teamName: "TeamB"
+  },
+  {
+    id: 7,
+    fullName: "James Bennet",
+    designation: "Angular Developer",
+    gender: "male",
+    teamName: "TeamC"
+  },
+  {
+    id: 8,
+    fullName: "Jessica Faye",
+    designation: "API Developer",
+    gender: "female",
+    teamName: "TeamC"
+  },
+  {
+    id: 9,
+    fullName: "Lita Stone",
+    designation: "C++ Developer",
+    gender: "female",
+    teamName: "TeamC"
+  },
+  {
+    id: 10,
+    fullName: "Daniel Young",
+    designation: "Python Developer",
+    gender: "male",
+    teamName: "TeamD"
+  },
+  {
+    id: 11,
+    fullName: "Adrian Jacobs",
+    designation: "Vue Developer",
+    gender: "male",
+    teamName: "TeamD"
+  },
+  {
+    id: 12,
+    fullName: "Devin Monroe",
+    designation: "Graphic Designer",
+    gender: "male",
+    teamName: "TeamD"
+  }]);
+
+  const handleChangeTeam = (e) => {
+    setSelectedTeam(e.target.value)
+  }
+
+  const handleShadow = (e) => {
+    const transformed = employees.map(employee => employee.id == parseInt(e.currentTarget.id) ? employee.teamName == selectedTeam ? {...employee, teamName: ""} : {...employee, teamName: selectedTeam} : employee);
+    setEmployees(transformed)
+  }
+
   return (
     <>
-     <Header />
-     <Employee />
+     <Header selectedTeam={selectedTeam} teamMemberCount={employees.filter((employee) => employee.teamName == selectedTeam).length} />
+     <Employee handleChangeTeam={handleChangeTeam} handleShadow={handleShadow} employees={employees} selectedTeam={selectedTeam} />
      <Footer /> 
     </>
   )
--- teamMember/src/Header.jsx
+++ teamMember/src/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({selectedTeam, teamMemberCount}) => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='text-center text-3xl font-bold m-3'>
+      <h1>Team Member Allocation</h1>
+      <h2>{selectedTeam} has {teamMemberCount} Members</h2>
     </div>
   )
 }
