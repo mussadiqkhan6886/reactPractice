a3f03139addd3195f41ef9261a0610e7b1ea428f
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-05-15 02:03:05 +0500

    started with router dom

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import StockDetailPage from './pages/StockDetailPage'
+import StockOverviewPage from './pages/StockOverviewPage'
 
 const App = () => {
+
+  // const api = 'd0ig4qpr01qrfsafuuigd0ig4qpr01qrfsafuuj0';
+
   return (
-    <div>
-      
-    </div>
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<StockOverviewPage />} />
+          <Route path='/details/:symbol' element={<StockDetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   )
 }
 
