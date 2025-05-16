749cc88e1a958259fa0b243b7d1e0984f5ad532a
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-05-16 19:10:45 +0500

    starting

--- dashboard/src/pages/StockOverviewPage.jsx
+++ dashboard/src/pages/StockOverviewPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import StockList from '../components/StockList'
+import AutoComplete from '../components/AutoComplete'
 
 const StockOverviewPage = () => {
   return (
     <div>
       <h1>StockOverview Page</h1>
+      <AutoComplete />
       <StockList />
     </div>
   )
