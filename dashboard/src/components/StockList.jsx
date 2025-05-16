4af971c877b1a5047bc165b58ed98d8705334663
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-05-16 19:10:00 +0500

    starting search comp

--- dashboard/src/components/StockList.jsx
+++ dashboard/src/components/StockList.jsx
@@ -1,43 +1,80 @@
-import React, { useEffect, useState } from 'react'
-import finnHub from '../apis/finnHub'
+import React, { useEffect, useState } from 'react';
+import finnHub from '../apis/finnHub';
 
 const StockList = () => {
-    const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZ"])
-    const [stock, setStock] = useState();
+  const [watchList, setWatchList] = useState(['GOOGL', 'MSFT', 'AMZN']);
+  const [stock, setStock] = useState([]);
 
-    useEffect(() => {
-        let isMounted = true
+  const changeColor = (change) => {
+    return change > 0 ? 'text-green-600' : 'text-red-600';
+  };
 
-        const fetchData = async () => {
-            try {
-                const responses = await Promise.all(
-                    watchList.map((symbol) => 
-                        finnHub.get("/quote", {
-                            params: { symbol }
-                        })
-                    )
-                )
-                console.log(responses)
-                if (isMounted) {
-                    setStock(responses)
-                }
-            } catch (err) {
-                console.error(err)
-            }
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      try {
+        const responses = await Promise.all(
+          watchList.map((symbol) =>
+            finnHub.get('/quote', {
+              params: { symbol },
+            })
+          )
+        );
+        const data = responses.map((resp) => ({
+          data: resp.data,
+          symbol: resp.config.params.symbol,
+        }));
+
+        if (isMounted) {
+          setStock(data);
         }
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-        fetchData()
+    fetchData();
 
-        return () => {
-            isMounted = false
-        }
-    }, [])
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   return (
     <div>
       <h1>Stock List</h1>
+      <table className='m-2'>
+        <thead className='border-b-1'>
+          <tr>
+            <th className='p-3 px-5'>Name</th>
+            <th className='p-3 px-5'>Last</th>
+            <th className='p-3 px-5'>Chg</th>
+            <th className='p-3 px-5'>Chg%</th>
+            <th className='p-3 px-5'>High</th>
+            <th className='p-3 px-5'>Low</th>
+            <th className='p-3 px-5'>Open</th>
+            <th className='p-3 px-5'>Pclose</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stock.map((data) => (
+            <tr className='p-10' key={data.symbol}>
+              <th className='text-left px-5'>{data.symbol}</th>
+              <td  className='p-1 px-5 text-left'>{data.data.c}</td>
+              <td className={changeColor(data.data.d)}>{data.data.d} {data.data.d > 0 ? <i class="fa-solid fa-arrow-up"></i> : <i class="fa-solid fa-arrow-down"></i>}</td>
+              <td className={changeColor(data.data.dp)}>{data.data.dp} {data.data.dp > 0 ? <i class="fa-solid fa-arrow-up"></i> : <i class="fa-solid fa-arrow-down"></i>}</td>
+              <td  className='p-1 px-5 text-left'>{data.data.h}</td>
+              <td className='p-1 px-5 text-left' >{data.data.l}</td>
+              <td className='p-1 px-5 text-left' >{data.data.o}</td>
+              <td className='p-1 px-5 text-left' >{data.data.pc}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default StockList
+export default StockList;
